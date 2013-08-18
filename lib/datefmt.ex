f0dcd75baee5b954da0cca83ad73a4f8d3e80f56
Alexei Sholik <alcosholik@gmail.com>
2013-08-18 16:36:39 +0300

    Start working on the formatting code

--- lib/datefmt.ex
+++ lib/datefmt.ex
@@ -2,8 +2,18 @@ defmodule DateFmt do
   def format(date, fmt) do
     case do_validate(fmt) do
       { :ok, parts } ->
-        # ...
-        :ok
+        {{year,_month,_day}, {_hour,_min,_sec}} = Date.local(date)
+        output = Enum.reduce(parts, [], fn
+          ({:YYYY, mod}, acc) ->
+            [acc, mod && :io_lib.format("~4..#{mod}B", [year]) || "#{year}"]
+
+          ({:YY, mod}, acc) ->
+            y = rem year, 100
+            [acc, mod && :io_lib.format("~2..#{mod}B", [y]) || "#{y}"]
+
+          (bin, acc) when is_binary(bin) ->
+            [acc, bin]
+        end) |> String.from_char_list
 
       error -> error
     end
@@ -17,6 +27,7 @@ defmodule DateFmt do
   end
 
   def parse(date, fmt) do
+    IO.puts "#{date} #{fmt}"
   end
 
   def validate(fmt) do
@@ -32,7 +43,7 @@ defmodule DateFmt do
   end
 
   defp do_validate("", _, parts, acc) do
-    { :ok, List.flatten([parts, {:bin, String.from_char_list!(acc)}]) }
+    { :ok, List.flatten([parts, String.from_char_list!(acc)]) }
   end
 
   defp do_validate("{{" <> rest, pos, parts, acc) do
@@ -44,16 +55,16 @@ defmodule DateFmt do
   end
 
   defp do_validate("{" <> rest, pos, parts, acc) do
-    case get_flag(rest, [], pos) do
-      { :ok, flag, rest } ->
+    case get_flag(rest, []) do
+      { flag, rest } ->
         case validate_flag(flag) do
-          :ok ->
-            new_parts = [parts, {:bin, String.from_char_list!(acc)}, {:flag, flag}]
+          {_,_}=new_flag ->
+            new_parts = [parts, String.from_char_list!(acc), new_flag]
             do_validate(rest, pos + size(flag), new_parts, [])
           :error ->
             { :error, "bad flag at #{pos+1}" }
         end
-      error -> error
+      :error -> { :error, "missing } (starting at #{pos})" }
     end
   end
 
@@ -65,29 +76,45 @@ defmodule DateFmt do
     do_validate(rest, pos+1, parts, [acc, c])
   end
 
-  defp get_flag("}" <> rest, acc, pos) do
-    { :ok, iolist_to_binary(acc), rest, pos }
+  defp get_flag("}" <> rest, acc) do
+    { iolist_to_binary(acc), rest }
   end
 
-  defp get_flag("", _, pos) do
-    { :error, "missing } (starting at #{pos})" }
+  defp get_flag("", _) do
+    :error
   end
 
-  defp get_flag(<<c :: utf8, rest :: binary>>, acc, pos) do
-    get_flag(rest, [acc, c], pos)
+  defp get_flag(<<c :: utf8, rest :: binary>>, acc) do
+    get_flag(rest, [acc, c])
   end
 
   defp validate_flag("0" <> flag) do
-    do_validate_flag(flag)
+    do_validate_flag(flag, "0")
   end
 
   defp validate_flag("_" <> flag) do
-    do_validate_flag(flag)
+    do_validate_flag(flag, " ")
   end
 
-  defp do_validate_flag(flag) when flag in ["Y", "y", "m", "h", "s"] do
-    :ok
+  defp validate_flag(flag) do
+    do_validate_flag(flag, nil)
   end
 
-  defp do_validate_flag(flag), do: :error
+  defp do_validate_flag(flag, nil)
+        when flag in ["Mshort", "Mfull",
+                      "WDshort", "WDfull",
+                      "am", "AM",
+                      "ZZZZ", "ZZ:ZZ"] do
+    { binary_to_atom(flag), nil }
+  end
+
+  defp do_validate_flag(flag, modifier)
+        when flag in ["YYYY", "YY", "M",
+                      "D", "Dord0", "Dord1",
+                      "W0", "W1",
+                      "h24", "h12", "m", "s"] do
+    { binary_to_atom(flag), modifier }
+  end
+
+  defp do_validate_flag(_, _), do: :error
 end
--- test/datefmt_test.exs
+++ test/datefmt_test.exs
@@ -1,6 +1,20 @@
 defmodule DateFmtTest do
   use ExUnit.Case
 
+  test :format_year do
+    date = Date.from({{2013,8,18}, {16,28,27}}, :utc)
+    old_date = Date.from({{3,8,18}, {16,28,27}}, :utc)
+
+    assert { :ok, "a2013b" } = DateFmt.format(date, "a{YYYY}b")
+
+    assert { :ok, "a3b" } = DateFmt.format(old_date, "a{YYYY}b")
+    assert { :ok, "a0003b" } = DateFmt.format(old_date, "a{0YYYY}b")
+    assert { :ok, "a   3b" } = DateFmt.format(old_date, "a{_YYYY}b")
+    assert { :ok, "a3b" } = DateFmt.format(old_date, "a{YY}b")
+    assert { :ok, "a03b" } = DateFmt.format(old_date, "a{0YY}b")
+    assert { :ok, "a 3b" } = DateFmt.format(old_date, "a{_YY}b")
+  end
+
   test :validate do
     assert :ok = DateFmt.validate ""
     assert :ok = DateFmt.validate "abc"
@@ -13,11 +27,18 @@ defmodule DateFmtTest do
     assert {:error, "extraneous } at 4"} = DateFmt.validate "abc } def"
   end
 
-  test :do_validate do
-    assert {:ok,[bin: ""]} = DateFmt.do_validate ""
-    assert {:ok,[bin: "abc"]} = DateFmt.do_validate "abc"
-    assert {:ok,[bin: "Use {{ as oft{{en as you like{{"]} = DateFmt.do_validate "Use {{ as oft{{en as you like{{"
-    assert {:ok,[bin: "Same go}}es for }}"]} = DateFmt.do_validate "Same go}}es for }}"
-    assert {:ok,[bin: "{{abc}}"]} = DateFmt.do_validate "{{abc}}"
+  test :do_validate_bins do
+    assert {:ok,[""]} = DateFmt.do_validate ""
+    assert {:ok,["abc"]} = DateFmt.do_validate "abc"
+    assert {:ok,["Use {{ as oft{{en as you like{{"]} = DateFmt.do_validate "Use {{ as oft{{en as you like{{"
+    assert {:ok,["Same go}}es for }}"]} = DateFmt.do_validate "Same go}}es for }}"
+    assert {:ok,["{{abc}}"]} = DateFmt.do_validate "{{abc}}"
+  end
+
+  test :do_validate_year do
+    assert {:ok,["", {:YYYY,nil}, ""]} = DateFmt.do_validate "{YYYY}"
+    assert {:ok,["", {:YYYY,"0"}, ""]} = DateFmt.do_validate "{0YYYY}"
+    assert {:ok,["", {:YYYY," "}, ""]} = DateFmt.do_validate "{_YYYY}"
+    assert {:error, "bad flag at 1"} = DateFmt.do_validate "{-YYYY}"
   end
 end
